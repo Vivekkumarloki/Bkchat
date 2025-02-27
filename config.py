dad67fb55178244c822c2d5abe7880c5243b6efd
AFK-HOTTY <musicnamoh@gmail.com>
2025-02-27 23:31:22 +0530

    Update config.py

--- config.py
+++ config.py
@@ -12,10 +12,10 @@ BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
 OWNER_ID = int(getenv("OWNER_ID", "5551813927"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/amritraj78/KING-CHATBOT")
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AFK-HOTTY/nainxchatbot")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "nainbots"
-UPDATE_CHNL = "nainsupport"
+SUPPORT_GRP = "Nainsupport"
+UPDATE_CHNL = "Nainbots"
 OWNER_USERNAME = "faizog"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
