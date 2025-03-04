03888fffb7098dda94d96802e1eef76a4bc81681
Vivekkumarloki <hp1030476@gmail.com>
2025-03-04 21:54:06 +0530

    ?

--- config.py
+++ config.py
@@ -4,19 +4,19 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_ID = "6435225"
+API_ID = "22475741"
 # -------------------------------------------------------------
-API_HASH = "4e984ea35f854762dcde906dce426c2d"
+API_HASH = "1a217be71a0225e0a678af286c211f8a"
 # --------------------------------------------------------------
 BOT_TOKEN = getenv("BOT_TOKEN", None)
 STRING1 = getenv("STRING_SESSION", None)
 MONGO_URL = getenv("MONGO_URL", None)
-OWNER_ID = int(getenv("OWNER_ID", "5551813927"))
-UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/AFK-HOTTY/nainxchatbot")
+OWNER_ID = int(getenv("OWNER_ID", "7250012103"))
+UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Vivekkumarloki/Bkchat")
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
-SUPPORT_GRP = "Nainsupport"
-UPDATE_CHNL = "Nainbots"
-OWNER_USERNAME = "faizog"
+SUPPORT_GRP = "BRANDED_WORLD"
+UPDATE_CHNL = "BRANDED_PAID_CC"
+OWNER_USERNAME = "BRANDEDKING8"
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", "")
     
