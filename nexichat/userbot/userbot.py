c2868eb8e0544f1ed4884ae6133833c02fa116d3
Vivekkumarloki <hp1030476@gmail.com>
2025-03-04 21:50:34 +0530

    ?

--- nexichat/userbot/userbot.py
+++ nexichat/userbot/userbot.py
@@ -24,8 +24,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("Nainsupport")
-                await self.one.join_chat("Nainsupport")
+                await self.one.join_chat("BRANDED_WORLD")
+                await self.one.join_chat("BRANDRD_BOT")
                
 
             except:
